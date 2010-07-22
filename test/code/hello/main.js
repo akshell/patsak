4284ad6b4456f57bd3ce9925c41af826049f7bf6
korenyushkin <anton@akshell.com>
2010-07-22 12:35:26 +0400

    Fixed the hello app.

--- test/code/hello/main.js
+++ test/code/hello/main.js
@@ -2,10 +2,12 @@
 
 
 exports.handle = function (socket) {
-  socket.send(
+  socket.write(
     'HTTP/1.1 200 OK\r\n' +
     'Content-Type: text/plain\r\n' +
     'Content-Length: 12\r\n' +
     '\r\n' +
     'Hello world!');
+  socket.shutdown('send');
+  socket.read();
 };
